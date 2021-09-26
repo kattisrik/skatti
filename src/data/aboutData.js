6b2d33fac22f5b07536b4febc246117fcfb4fd09
kattisrik <srikanthkatti.katti@gmail.com>
2021-09-27 00:29:00 +0530

    update data

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,7 @@
 export const aboutData = {
   title: "Who I am",
   description1:
-    "My name's Srikanth. I'm a software developer based in Bengaluru, India.",
+    "My name's Srikanth. A Fullstack developer based in Bengaluru, India, with 2+ years experience, loves solving interesting problems using latest technology.",
   description2:
     "During the day, I work as a developer at a product based company and in the evening, I work on freelance projects and utilize the time to build my own products. I spend my leisure hours reading philosophy. Currently I'm also trying to build a SaaS product. I love to learn and explore new arenas.",
   image: 2,
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ export const headerData = {
   name: "Srikanth Katti",
   title: "जिज्ञासु {Pursuit of knowledge}",
   desciption:
-    "A Fullstack developer with 2+ years experience, loves solving interesting problems using latest technology. Looks for an opportunity to work in a challenging environment to improve and utilize my knowledge and problem solving skills for making a contribution towards the growth of the organization and society.",
+    "Looks for an opportunity to work in a challenging environment to improve and utilize my knowledge and problem solving skills for making a contribution towards the growth of the organization and society.",
   image:
     "https://i.ibb.co/YWJSLTv/Whats-App-Image-2021-09-25-at-7-58-55-PM.jpg",
   resumePdf: resume,

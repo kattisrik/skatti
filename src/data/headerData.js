f528dc67171b4bd53898af72fbdc53172d297c59
kattisrik <srikanthkatti.katti@gmail.com>
2021-09-28 09:51:54 +0530

    update

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,9 +2,9 @@ import resume from "../assets/pdf/resume.pdf";
 
 export const headerData = {
   name: "Srikanth Katti",
-  title: "जिज्ञासु {Pursuit of knowledge}",
+  title: "जिज्ञासु {In pursuit of knowledge}",
   desciption:
-    "Looks for an opportunity to work in a challenging environment to improve and utilize my knowledge and problem solving skills for making a contribution towards the growth of the organization and society.",
+    "Looking for an opportunity to work in a challenging environment to improve and utilize my knowledge and problem solving skills for making a contribution towards the growth of the organization and society.",
   image:
     "https://i.ibb.co/YWJSLTv/Whats-App-Image-2021-09-25-at-7-58-55-PM.jpg",
   resumePdf: resume,

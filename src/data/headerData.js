ccae671a24f4da3a3ed878784b0aff9de9faab85
kattisrik <srikanthkatti.katti@gmail.com>
2021-09-28 22:18:45 +0530

    update home page data

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ export const headerData = {
   name: "Srikanth Katti",
   title: "जिज्ञासु {In pursuit of knowledge}",
   desciption:
-    "Looking for an opportunity to work in a challenging environment to improve and utilize my knowledge and problem solving skills for making a contribution towards the growth of the organization and society.",
+    "I enjoy working in a challenging environment that enhances and utilizes my knowledge and problem solving skills. Hoping to contribute towards the growth of the organization and society.",
   image:
     "https://i.ibb.co/YWJSLTv/Whats-App-Image-2021-09-25-at-7-58-55-PM.jpg",
   resumePdf: resume,
